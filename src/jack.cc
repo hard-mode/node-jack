937b63e2d34976ea9a774722b604aa4f3517e30b
Adam Avramov <fallenblood@gmail.com>
2015-04-13 22:29:10 +0300

    Add stuff

--- src/jack.cc
+++ src/jack.cc
@@ -18,16 +18,6 @@ class Client : public ObjectWrap {
 
   private:
 
-    jack_client_t * client;
-
-    Client (const char * name) {
-      client = jack_client_open(name, JackNullOption, NULL);
-    }
-
-    ~Client () {
-      jack_client_close(client);
-    }
-
     static NAN_METHOD(New) {
       NanScope();
       Client * obj  = new Client(*NanUtf8String(args[0]));
@@ -35,7 +25,54 @@ class Client : public ObjectWrap {
       NanReturnValue(args.This());
     }
 
+    jack_client_t * client;
+
+    Client (const char * name) {
+      client = jack_client_open(name, JackNullOption, NULL);
+      jack_set_client_registration_callback(
+        client, client_registered, this);
+      jack_set_port_registration_callback(
+        client, port_registered, this);
+      jack_activate(client);
+    }
+
+    ~Client () {
+      jack_client_close(client);
+    }
+
+    static void client_registered
+      ( const char * name
+      , int          reg
+      , void       * client_ptr)
+    {
+      Client* client = static_cast<Client*> client_ptr;
+      if (reg) {
+        client->emitEvent("client-registered", name); 
+      } else {
+        client->emitEvent("client-unregistered", name); 
+      }
+    }
+
+    static void port_registered
+      ( jack_port_id_t port
+      , int            reg
+      , void         * client_ptr)
+    {
+      Client* client = static_cast<Client*> client_ptr;
+      if (reg) {
+        client->emitEvent("port-registered"); 
+      } else {
+        client->emitEvent("port-unregistered"); 
+      }
+    }
+
   public:
+
+    NAN_METHOD (emitEvent) {
+      NanScope();
+      fprintf(stderr, "EVENT");
+      NanReturnUndefined();
+    }
 
     static void Init() {
       Local<FunctionTemplate> t =
@@ -47,10 +84,17 @@ class Client : public ObjectWrap {
 
 };
 
+//void * client_registered ( const char * name
+                         //, int          reg
+                         //, void       * client_ptr ) {
+  //printf("CLIENT REGISTERED 1");
+  //static_cast<Client*>(client_ptr)->emitEvent("client-registered");
+//}
+
 void init (Handle<Object> exports) {
   Client::Init();
   Local<FunctionTemplate> handle = NanNew(constructor);
-  exports->Set(NanSymbol("Client"), handle->GetFunction());
+  exports->Set(NanNew("Client"), handle->GetFunction());
 }
 
 NODE_MODULE(jack, init)
--- test/test.js
+++ test/test.js
@@ -1,5 +1,5 @@
 var jack = require('../build/Release/jack.node');
-
+process.stdin.resume();
 console.log(jack);
 //console.log(jack.Client('foo', {}));
 console.log(new jack.Client('test', {}));

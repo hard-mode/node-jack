86bf467048d7fa420e83f462260c4343c23b6edc
Adam Avramov <fallenblood@gmail.com>
2015-04-14 04:07:00 +0300

    still ~nothing

--- test/test.js
+++ test/test.js
@@ -1,5 +1,14 @@
 var jack = require('../build/Release/jack.node');
+console.log("1. JACK", jack);
+
+console.log("2. Class", jack.Client)
+
+console.log("3. Proto", jack.Client.prototype)
+
+var client = new jack.Client('test');
+console.log("4. Instance", client);
+
 process.stdin.resume();
-console.log(jack);
-//console.log(jack.Client('foo', {}));
-console.log(new jack.Client('test', {}));
+client.onclient = function () { console.log("CLIENT!") }
+client.onport   = function () { console.log("PORT!")   }
+console.log("5. onclient, onport");

979cebbef77b32ee7d93c9f97d12f8cdcc337a96
Adam Avramov <fallenblood@gmail.com>
2015-04-15 01:00:13 +0300

    even cleaner callback; how do i specify type

--- test/test.js
+++ test/test.js
@@ -10,10 +10,23 @@ console.log("4. Instance", client);
 
 process.stdin.resume();
 function cb () { console.log ("CALLBACK", this, arguments ) }
-client.on('client-registered',   cb);
-client.on('client-unregistered', cb);
-client.on('port-registered',     cb);
-client.on('port-unregistered',   cb);
+
+client.on('client-registered', function () {
+  console.log("Client registered", arguments)
+});
+
+client.on('client-unregistered', function () {
+  console.log("Client unregistered", arguments)
+});
+
+client.on('port-registered', function () {
+  console.log("Port registered", arguments)
+});
+
+client.on('port-unregistered', function () {
+  console.log("Port registered", arguments)
+});
+
 console.log("5. Callbacks");
 
 require('child_process').spawn('jack_simple_client');
